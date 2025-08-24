33a67040a2a7dcc1f446227ad88b918c5374d56e
alaamahmoudcode200-maker <alaamahmoudcode200@gmail.com>
2025-08-24 12:29:06 +0300

    eidet1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,8 @@
 import HomePage from "./home/page";
 export default function Home() {
   return (
-    <div className="font-sans  h-screen ">
       <main className=" bg-white ">
          <HomePage/>
       </main>
-   
-    </div>
   );
 }
