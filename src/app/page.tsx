c62754198f984b748962110a19254e2d5d5b34a5
alaamahmoudcode200-maker <alaamahmoudcode200@gmail.com>
2025-08-24 12:18:52 +0300

    eidet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import HomePage from "./home/page";
 export default function Home() {
   return (
     <div className="font-sans  bg-white grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex bg-white flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <main className=" bg-white ">
          <HomePage/>
       </main>
    
